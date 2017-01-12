5d95d63cc85c3ec0f7016993a3aef2034b605499
LuHu <luhu0105@gmail.com>
2017-01-12 18:01:42 +0800

    add by LuHu1.12(1) in the company

---
+++ JavaScriptTest/test.js
@@ -0,0 +1,59 @@
+// JavaScript Document
+a="hello javascript!";
+alert(a);
+
+
+
+
+
+
+function myFunction()
+	{
+		x=document.getElementById("luhu");
+		x.innerHTML="修改过后的div!";
+		document.getElementById("p").innerHTML="哈哈，段落被我修改了";
+		document.getElementById("div").innerHTML="哈哈，div也被我修改了";
+		document.getElementById("h").innerHTML="哈哈，一号标题也被我修改了";
+		var x = 34;
+		var y = "e";
+		var z = x+y;
+		document.write(z);
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
